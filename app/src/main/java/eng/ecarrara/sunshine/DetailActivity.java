2d82595b7ded5098fd60a1b7ef828cd39c156f73
Eduardo Carrara <ecarrara.araujo@gmail.com>
2014-11-18 07:44:17 -0200

    Lesson 3: Updating DetailActivity to use fragment.
    Signed-off-by: Eduardo Carrara <ecarrara.araujo@gmail.com>

--- app/src/main/java/eng/ecarrara/sunshine/DetailActivity.java
+++ app/src/main/java/eng/ecarrara/sunshine/DetailActivity.java
@@ -1,10 +1,14 @@
 package eng.ecarrara.sunshine;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 
@@ -14,17 +18,18 @@ public class DetailActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        String weatherDescriptionText = this.getIntent().getStringExtra(Intent.EXTRA_TEXT);
-        TextView weatherTextTxtView = (TextView) findViewById(R.id.weatherInfoText);
-        weatherTextTxtView.setText(weatherDescriptionText);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.detail, menu);
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
         return true;
     }
 
@@ -34,11 +39,33 @@ public class DetailActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Intent settingsIntent = new Intent(this, SettingsActivity.class);
-            startActivity(settingsIntent);
             return true;
         }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            String weatherDescriptionText = getActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT);
+            TextView weatherTextTxtView = (TextView) rootView.findViewById(R.id.weatherInfoText);
+            weatherTextTxtView.setText(weatherDescriptionText);
+
+            return rootView;
+        }
     }
 }
