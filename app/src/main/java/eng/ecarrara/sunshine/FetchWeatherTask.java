0b61ca2a01231e7021b02cb4f25bae4a23ef6412
Eduardo Carrara <ecarrara.araujo@gmail.com>
2014-11-26 14:05:10 -0200

    Fix call for json parsing on FetchWeatherTask
    Signed-off-by: Eduardo Carrara <ecarrara.araujo@gmail.com>

--- app/src/main/java/eng/ecarrara/sunshine/FetchWeatherTask.java
+++ app/src/main/java/eng/ecarrara/sunshine/FetchWeatherTask.java
@@ -154,18 +154,14 @@ public class FetchWeatherTask extends AsyncTask<String, Void, Void> {
             }
         }
 
-        return null;
-    }
+        try {
+            getWeatherDataFromJson(forecastJsonStr, period, location);
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, e.getMessage(), e);
+            e.printStackTrace();
+        }
 
-    /* The date/time conversion code is going to be moved outside the asynctask later,
-     * so for convenience we're breaking it out into its own method now.
-     */
-    private String getReadableDateString(long time) {
-        // Because the API returns a unix timestamp (measured in seconds),
-        // it must be converted to milliseconds in order to be converted to valid date.
-        Date date = new Date(time * 1000);
-        SimpleDateFormat format = new SimpleDateFormat("E, MMM d");
-        return format.format(date).toString();
+        return null;
     }
 
     /**
